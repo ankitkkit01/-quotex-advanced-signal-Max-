a5969c36c26d18647d9da10efbabdee362ffd3b6
ankitkkit01 <akhimanshya@gmail.com>
2025-06-23 14:11:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# main.py
-
-from code.quotex_bot import main
+import asyncio
+from code.quotex_bot import QuotexSignalBot
 
 if __name__ == "__main__":
-    main()
+    bot = QuotexSignalBot(token="YOUR_TELEGRAM_BOT_TOKEN")
+    asyncio.run(bot.run())
